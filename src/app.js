2c0629beb2d3ed69aa3157a1bc05e1dd3f9146d7
Justin Yao Du <justinyaodu@gmail.com>
2023-05-18 21:31:59 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const env = require("./env");
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: env.SLACK_BOT_TOKEN,
+  signingSecret: env.SLACK_SIGNING_SECRET,
+  port: env.PORT,
+});
+
+app.event("app_mention", async ({ event, say }) => {
+  say(`Hello world! <@${event.user}> said: ${event.text}`);
+});
+
+async function main() {
+  await app.start();
+}
+
+main();
---
+++ src/env.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+/**
+ * @returns {number}
+ */
+function getPort() {
+  const port = process.env.PORT;
+  if (port !== undefined) {
+    const parsed = parseInt(port, 10);
+    if (!Number.isNaN(parsed)) {
+      return parsed;
+    }
+  }
+  return 3000;
+}
+
+const env = {
+  PORT: getPort(),
+  SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN,
+  SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
+};
+
+module.exports = env;
